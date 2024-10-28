d2846669c03eae74f13c9c637f7bb04a50af2c5e
SWETHA-0945 <swethamk45@gmail.com>
2024-10-28 13:33:38 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const specificRoutes = require('./routes/specificRoutes');
 dotenv.config();
 
 const app = express();
-app.use(cors({}));
+app.use(cors({ origin: '*' }));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
